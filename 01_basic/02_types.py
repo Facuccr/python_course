f362f0a9a53ef033912bf4281b8b460789c5a34c
Facundo Cristaldo <facundocristaldo146@gmail.com>
2026-02-03 13:35:32 -0300

    tipos de datos basicos

---
+++ 01_basic/02_types.py
@@ -0,0 +1,25 @@
+### 
+# types:
+# int, float, complex, str, bool, NoneType, 
+# list, tuple, dict, range, set...
+###
+
+print("int:")
+# print(type(10))
+# print(-5)
+
+print("float:")
+# print(type(5.0))
+# print(type(3.14))
+# print(type(1e3)) 
+
+print("complex:")
+# print(type(1 + 2j))
+
+print("bool:")
+# print(type(True))
+# print(type(False))
+# print(type(1 > 2))
+
+print("NoneType:")
+print(type(None))
