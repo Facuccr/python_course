bd8c0f96e9503916ee8adf41a2a5a6864c5aa150
Facundo Cristaldo <facundocristaldo146@gmail.com>
2026-02-03 13:17:46 -0300

    mostrar mensajes en la consola

---
+++ 01_basic/01_print.py
@@ -0,0 +1,14 @@
+print("Hola, Mundo")
+print('Hola, Mundo')
+
+print("Phyton", "es", "god")
+
+print("Python", "es", "god", sep="-")
+
+print("Esto se imprime", end=" ")
+print("en una linea")
+
+print("Esto se imprime", end="-")
+print("en una linea")
+
+print(42)
